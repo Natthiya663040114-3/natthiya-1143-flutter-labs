7c2169e593e41d056752f8b48d10bbc7b3f22d20
Natthiya <natthiya663040114@example.com>
2025-12-10 16:22:19 +0700

    Exercise3

--- natthiya_widgets_exercise/lib/1143_main_quote_container_card.dart
+++ natthiya_widgets_exercise/lib/1143_main_quote_container_card.dart
@@ -14,23 +14,19 @@ class QuoteContainerCard extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My Favorite Quote'),
-          backgroundColor: Colors.blue, // Theme color
+          backgroundColor: Colors.deepPurple,
           foregroundColor: Colors.white,
         ),
         body: Column(
           children: [
-            Expanded(
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 100, 0, 0),
               child: Center(
                 child: ConstrainedBox(
                   constraints: const BoxConstraints(maxHeight: 500),
                   child: Image.asset(
                     'assets/new.jpg',
                     fit: BoxFit.contain,
-                    errorBuilder: (context, error, stackTrace) {
-                      return const Center(
-                        child: Text('Please add an image to assets/images/quote.jpg'),
-                      );
-                    },
                   ),
                 ),
               ),
@@ -38,7 +34,6 @@ class QuoteContainerCard extends StatelessWidget {
 
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 30.0),
-
               padding: const EdgeInsets.all(20.0),
 
               decoration: BoxDecoration(
@@ -48,18 +43,34 @@ class QuoteContainerCard extends StatelessWidget {
                   width: 3.0,
                 ),
                 borderRadius: BorderRadius.circular(10),
+                boxShadow: [BoxShadow(blurRadius: 7,
+                                      offset: const Offset(0,3))]
               ),
 
-              child: const Text(
-                "Natthiya",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black, // Text color contrasting with pink background
-                ),
-              ),
-            ),
+              child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Natthiya",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+
+                      const SizedBox(width: 10),
+                      const Text(
+                        "Chakaew",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
           ],
         ),
       ),
